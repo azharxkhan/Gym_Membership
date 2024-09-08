3de9c97c6fb7869363c1c0390f268542b5f41cc9
Azhar <akhan023@student.wethinkcode.co.za>
2024-09-08 11:44:45 +0200

    edited mainclass

--- src/main/java/gymApp/MainClass.java
+++ src/main/java/gymApp/MainClass.java
@@ -3,13 +3,22 @@ package gymApp;
 import gymApp.controller.LoginController;
 import gymApp.service.AuthService;
 
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
         AuthService authService = new AuthService();
         LoginController loginController = new LoginController(authService);
 
-        // Example usage
-        boolean isAuthenticated = loginController.login("Azhar", "CorrectPassword");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        boolean isAuthenticated = loginController.login(username, password);
         System.out.println("Authenticated: " + isAuthenticated);
     }
 }
