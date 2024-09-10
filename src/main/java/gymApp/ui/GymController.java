8c28d211daa20a8a170956ee9593280dab73597a
Azhar <akhan023@student.wethinkcode.co.za>
2024-09-10 23:00:16 +0200

    moved to ui

--- src/main/java/gymApp/ui/GymController.java
+++ src/main/java/gymApp/ui/GymController.java
@@ -1,4 +1,4 @@
-package gymApp;
+package gymApp.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/gymApp/ui/GymMembershipApp.java
+++ src/main/java/gymApp/ui/GymMembershipApp.java
@@ -1,4 +1,4 @@
-package gymApp;
+package gymApp.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
