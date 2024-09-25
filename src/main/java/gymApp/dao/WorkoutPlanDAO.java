e1f62a4d21ed3570318e3aee434cafb1e3492e77
Azhar <azhargamer15@gmail.com>
2024-09-25 20:16:08 +0200

    added rest of interface and doc strings

--- src/main/java/gymApp/dao/WorkoutPlanDAO.java
+++ src/main/java/gymApp/dao/WorkoutPlanDAO.java
@@ -1,10 +1,50 @@
 package gymApp.dao;
 
-import gymApp.model.WorkoutPlan;
 import java.util.List;
 
+import gymApp.model.WorkoutPlan;
+
+/**
+ * Data Access Object (DAO) interface for WorkoutPlan entities.
+ */
 public interface WorkoutPlanDAO {
+
+    /**
+     * Creates a new workout plan.
+     * 
+     * @param plan the workout plan to be created
+     * @return the created workout plan
+     */
     WorkoutPlan createWorkoutPlan(WorkoutPlan plan);
+
+    /**
+     * Retrieves all workout plans.
+     * 
+     * @return a list of all workout plans
+     */
     List<WorkoutPlan> getAllWorkoutPlans();
+
+    /**
+     * Updates an existing workout plan.
+     * 
+     * @param plan the workout plan to be updated
+     * @return true if the update is successful, false otherwise
+     */
+    boolean updateWorkoutPlan(WorkoutPlan plan);
+
+    /**
+     * Deletes a workout plan by its ID.
+     * 
+     * @param planId the ID of the workout plan to be deleted
+     * @return true if the deletion is successful, false otherwise
+     */
     boolean deleteWorkoutPlan(int planId);
+
+    /**
+     * Finds a workout plan by its ID.
+     * 
+     * @param planId the ID of the workout plan to be found
+     * @return the workout plan with the given ID, or null if not found
+     */
+    WorkoutPlan findWorkoutPlanById(int planId);
 }
