db7593d9266e953154714a4d747395bad28aef8c
Azhar <azhargamer15@gmail.com>
2024-10-26 17:03:56 +0200

    made doc strings

--- src/main/java/gymApp/model/ProgressTracking.java
+++ src/main/java/gymApp/model/ProgressTracking.java
@@ -1,5 +1,8 @@
 package gymApp.model;
 
+/**
+ * Represents the progress tracking of a member's exercise activities.
+ */
 public class ProgressTracking {
     private int progressId;
     private int memberId;
@@ -9,62 +12,129 @@ public class ProgressTracking {
     private int repsCompleted;
     private String notes;
 
+    /**
+     * Gets the unique identifier for this progress tracking entry.
+     *
+     * @return the progress ID.
+     */
     public int getProgressId() {
         return progressId;
     }
 
+    /**
+     * Sets the unique identifier for this progress tracking entry.
+     *
+     * @param progressId the new progress ID.
+     */
     public void setProgressId(int progressId) {
         this.progressId = progressId;
     }
 
+    /**
+     * Gets the ID of the member associated with this progress tracking entry.
+     *
+     * @return the member ID.
+     */
     public int getMemberId() {
         return memberId;
     }
 
+    /**
+     * Sets the ID of the member associated with this progress tracking entry.
+     *
+     * @param memberId the new member ID.
+     */
     public void setMemberId(int memberId) {
         this.memberId = memberId;
     }
 
+    /**
+     * Gets the date when the progress was recorded.
+     *
+     * @return the date of progress tracking.
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Sets the date when the progress was recorded.
+     *
+     * @param date the date of progress tracking.
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     * Gets the ID of the exercise associated with this progress tracking entry.
+     *
+     * @return the exercise ID.
+     */
     public int getExerciseId() {
         return exerciseId;
     }
 
+    /**
+     * Sets the ID of the exercise associated with this progress tracking entry.
+     *
+     * @param exerciseId the new exercise ID.
+     */
     public void setExerciseId(int exerciseId) {
         this.exerciseId = exerciseId;
     }
 
+    /**
+     * Gets the number of sets completed for the exercise.
+     *
+     * @return the number of sets completed.
+     */
     public int getSetsCompleted() {
         return setsCompleted;
     }
 
+    /**
+     * Sets the number of sets completed for the exercise.
+     *
+     * @param setsCompleted the new number of sets completed.
+     */
     public void setSetsCompleted(int setsCompleted) {
         this.setsCompleted = setsCompleted;
     }
 
+    /**
+     * Gets the number of repetitions completed for the exercise.
+     *
+     * @return the number of repetitions completed.
+     */
     public int getRepsCompleted() {
         return repsCompleted;
     }
 
+    /**
+     * Sets the number of repetitions completed for the exercise.
+     *
+     * @param repsCompleted the new number of repetitions completed.
+     */
     public void setRepsCompleted(int repsCompleted) {
         this.repsCompleted = repsCompleted;
     }
 
+    /**
+     * Gets any additional notes associated with this progress tracking entry.
+     *
+     * @return the notes.
+     */
     public String getNotes() {
         return notes;
     }
 
+    /**
+     * Sets any additional notes associated with this progress tracking entry.
+     *
+     * @param notes the new notes.
+     */
     public void setNotes(String notes) {
         this.notes = notes;
     }
-
-    
 }
-
