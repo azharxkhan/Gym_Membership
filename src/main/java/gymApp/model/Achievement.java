ae8f5a23a682781091512567767c6fbc73eb4755
Azhar <azhargamer15@gmail.com>
2024-11-20 19:10:19 +0200

    feat: create Achievement.java

---
+++ src/main/java/gymApp/model/Achievement.java
@@ -0,0 +1,43 @@
+package gymApp.model;
+
+public class Achievement {
+    private final int id;
+    private String description;
+    private boolean isAchieved;
+    private int rewardPoints;
+
+    public Achievement(int id, String description, int rewardPoints) {
+        this.id = id;
+        this.description = description;
+        this.isAchieved = false;
+        this.rewardPoints = rewardPoints;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIsAchieved() {
+        return isAchieved;
+    }
+
+    public void setIsAchieved(boolean isAchieved) {
+        this.isAchieved = isAchieved;
+    }
+
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+
+    public void setRewardPoints(int rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+}
